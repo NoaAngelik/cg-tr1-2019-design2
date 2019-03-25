82e2768258b1ecfe32a38c04b57d026107f99dec
Ana Angélica <aninha298.aa@gmail.com>
2019-03-25 09:41:39 -0300

    pegando as moedinhas com o passaro e colocando efeito

---
+++ Assets/script/DestroyWithDellay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWithDellay : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+   public float delay = 3f;
+
+   
+    void Start()
+    {
+        Destroy(gameObject, delay);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
