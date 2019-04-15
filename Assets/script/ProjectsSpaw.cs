e6df7de6c8c0c668d213e26ae984032cde35c79a
Ana Angélica <aninha298.aa@gmail.com>
2019-04-15 09:15:36 -0300

    colocando bolinhas q matam

---
+++ Assets/script/ProjectsSpaw.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectsSpaw : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    GameObject parent;
+
+    public GameObject predab;
+
+    public GameObject spawnPoint;
+    public float waitTime = 3f;
+
+    public float projectileSpeed = 3f;
+
+    public Vector3[] directions;
+
+        void Start()
+    {
+
+     parent = GameObject.Find("Tape");
+     StartCoroutine(Spawn());
+    }
+IEnumerator Spawn(){
+
+    while (true) {
+        yield return new WaitForSeconds(waitTime);
+        for(int i=0; i<directions.Length; i++) {
+
+            GameObject projectile = Instantiate(predab,spawnPoint.transform.position, Quaternion.identity);
+        projectile.transform.SetParent(parent.transform);
+        projectile.GetComponent<Rigidbody2D>().velocity = projectileSpeed * directions[i];
+        }
+    }
+}
+}
